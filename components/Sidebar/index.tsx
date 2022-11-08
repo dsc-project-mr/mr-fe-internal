76c48b71ae4fee1560203c7ca2527f97b143ac43
Ling Guan Ming <myself3216@gmail.com>
2022-11-08 21:31:58 +0800

    6 sidebar component (#8)
    * feat: Add Sidebar, HeaderTab and SubheaderTab
    
    * refactor: Move sidebar_data into separate file
    
    * feat: Add logout button and sidebar header
    
    * feat: UI feedback for selected tabs
    
    * style: Make sidebar persistent
    
    * feat: Add routes
    
    * chore: update README
    
    * fix: PR changes
    
    * Add memo to HeaderTab
    
    * Delete sidebar page
    
    Co-authored-by: wilinetan <wilinetan26@gmail.com>

---
+++ components/Sidebar/index.tsx
@@ -0,0 +1,76 @@
+import { Button, Drawer, List, ListItem, Typography } from '@mui/material'
+import { useState } from 'react'
+import HeaderTab from './HeaderTab'
+import { sidebarData } from '../../constants/sidebarData'
+import LogoutIcon from '@mui/icons-material/Logout'
+
+// Rename tabs
+// Hover
+// Select tabs ui feedback
+
+const DRAWER_WIDTH = 320
+
+const Sidebar = () => {
+  const [tabSelected, setTabSelected] = useState<string>('')
+
+  return (
+    <>
+      <Drawer
+        anchor="left"
+        open={true}
+        variant="persistent"
+        sx={{
+          width: DRAWER_WIDTH,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: DRAWER_WIDTH,
+            boxSizing: 'border-box',
+          },
+
+          /* 
+            This is to make sure the Log Out button remains at the bottom.
+          */
+          '& .MuiPaper-root.MuiDrawer-paper': {
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          },
+        }}
+      >
+        <List>
+          <ListItem>
+            <Typography
+              sx={{
+                color: 'text.secondary',
+                fontSize: 18,
+                fontWeight: 700,
+              }}
+            >
+              Mercy Relief Internal Portal
+            </Typography>
+          </ListItem>
+          {sidebarData.map((headerData, index) => (
+            <HeaderTab
+              key={index}
+              headerData={headerData}
+              tabSelected={tabSelected}
+              setTabSelected={setTabSelected}
+            />
+          ))}
+        </List>
+        <Button
+          variant="contained"
+          sx={{
+            width: '260px',
+            backgroundColor: '#1976D2',
+            marginBottom: '20px',
+          }}
+        >
+          <LogoutIcon />
+          LOG OUT
+        </Button>
+      </Drawer>
+    </>
+  )
+}
+
+export default Sidebar
