3a237fe0bcffb78226c918b48977510fcf301876
firwer <firwer@gmail.com>
2022-12-22 18:02:14 +0800

    added skeletal page with basic validation check, drag-n-drop upload and editor

--- pages/articles/create/index.tsx
+++ pages/articles/create/index.tsx
@@ -1,11 +1,20 @@
-import { Avatar, Button, Grid, TextField } from '@mui/material'
+import {
+  Avatar,
+  Button,
+  Grid,
+  TextField,
+} from '@mui/material'
 import { Editor } from '@tinymce/tinymce-react'
+import UploadFileIcon from '@mui/icons-material/UploadFile'
 import router from 'next/router'
 import React, { useState } from 'react'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
-import { FileUploader } from 'react-drag-drop-files'
-import UploadFileIcon from '@mui/icons-material/UploadFile'
-
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import DialogContent from '@mui/material/DialogContent'
+import DialogContentText from '@mui/material/DialogContentText'
+import DialogTitle from '@mui/material/DialogTitle'
+import { useDropzone } from 'react-dropzone'
 const styleduploadbox = {
   borderWidth: 'thin',
   borderStyle: 'dashed',
@@ -24,16 +33,80 @@ const styledtopbar = {
   justifyContent: 'space-between',
 }
 
-const fileTypes = ['JPG', 'PNG']
+const AlertDialog = ({
+  message,
+  open,
+  handleClose,
+}: {
+  message: string
+  open: boolean
+  handleClose: any
+}) => {
+  return (
+    <Dialog open={open} onClose={handleClose}>
+      <DialogTitle id="alert-dialog-title">{'Information'}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {message}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
 
 const CreateArticle = () => {
-  // const editorRef = useRef(null)
-  const [uploadedFile, setFile] = useState()
-  const handleChange = (uploadedFile: React.SetStateAction<undefined>) => {
-    setFile(uploadedFile)
+  const [open, setOpen] = React.useState(false)
+  const [openSave, setOpenSave] = React.useState(false)
+  const [title, setTitle] = useState('')
+  const [file, setFile] = useState<any[]>([])
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {
+      'image/*': [],
+    },
+    onDrop: (acceptedFiles) => {
+      setFile(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      )
+    },
+  })
+  const handleTextChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setTitle(event.target.value)
+  }
+
+  const handleOpen = () => {
+    setOpen(true)
+  }
+  const handleSaveOpen = () => {
+    setOpenSave(true)
+  }
+
+  const handleSaveClose = () => {
+    setOpenSave(false)
+  }
+  const handleClose = () => {
+    setOpen(false)
   }
   return (
     <>
+      <AlertDialog
+        message="Article Published Successfully!"
+        open={open}
+        handleClose={handleClose}
+      />
+      <AlertDialog
+        message="Article Saved Successfully!"
+        open={openSave}
+        handleClose={handleSaveClose}
+      />
       <div
         style={{
           display: 'flex',
@@ -50,12 +123,24 @@ const CreateArticle = () => {
         </div>
         <div>
           <div style={styledtopbar}>
-            <h6 style={styledtitle}>Title of Article</h6>
+            <h6 style={styledtitle}>
+              Title of Article<span style={{ color: 'red' }}>*</span>
+            </h6>
             <div>
-              <Button sx={{ m: 5 }} variant="contained" color="success">
+              <Button
+                sx={{ m: 5 }}
+                variant="contained"
+                color="success"
+                onClick={handleOpen}
+                disabled={file.length === 0 || title.length === 0}
+              >
                 Publish
               </Button>
-              <Button variant="contained" color="secondary">
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={handleSaveOpen}
+              >
                 Save
               </Button>
             </div>
@@ -65,42 +150,75 @@ const CreateArticle = () => {
             id="filled-hidden-label-normal"
             placeholder="Title of article"
             variant="filled"
+            onChange={(event) => handleTextChange(event)}
             fullWidth
             required
             InputProps={{ disableUnderline: true }}
           />
         </div>
         <div>
-          <h6 style={styledtitle}>Cover Photo</h6>
+          <h6 style={styledtitle}>
+            Cover Photo<span style={{ color: 'red' }}>*</span>
+          </h6>
         </div>
-        <FileUploader handleChange={handleChange} name="file" types={fileTypes}>
-          <Grid
-            sx={styleduploadbox}
-            container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
-            spacing={2}
-            columns={12}
-          >
-            <Grid item xs={12}>
-              <Avatar sx={{ bgcolor: '#bbdefb' }}>
-                <UploadFileIcon sx={{ color: '#1976D2' }} />
-              </Avatar>
-            </Grid>
-            <Grid item xs={8}>
-              <p>
-                <span style={{ textDecoration: 'underline', color: '#1976D2' }}>
-                  Click To Upload
-                </span>{' '}
-                or Drag and Drop
-              </p>
-              <p style={{ color: '#9e9e9e' }}>
-                SVG, PNG, JPG or GIF (max. 3MB)
-              </p>
-            </Grid>
-          </Grid>
-        </FileUploader>
+        {
+          <section className="container">
+            <div {...getRootProps({ className: 'dropzone' })}>
+              <input {...getInputProps()} />
+              <Grid
+                sx={styleduploadbox}
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                columns={12}
+              >
+                {file.length !== 0 ? (
+                  <img
+                    src={URL.createObjectURL(file[0])}
+                    height="250px"
+                    onLoad={() => {
+                      URL.revokeObjectURL(URL.createObjectURL(file[0]))
+                    }}
+                  />
+                ) : (
+                  <div>
+                    <Grid
+                      item
+                      xs={12}
+                      sx={{ display: 'flex', justifyContent: 'center' }}
+                    >
+                      <Avatar sx={{ bgcolor: '#bbdefb' }}>
+                        <UploadFileIcon
+                          sx={{
+                            color: '#1976D2',
+                          }}
+                        />
+                      </Avatar>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <p style={{ justifyContent: 'center' }}>
+                        <span
+                          style={{
+                            textDecoration: 'underline',
+                            color: '#1976D2',
+                          }}
+                        >
+                          Click To Upload
+                        </span>
+                        &nbsp;or Drag and Drop
+                      </p>
+                      <p style={{ color: '#9e9e9e' }}>
+                        SVG, PNG, JPG or GIF (max. 3MB)
+                      </p>
+                    </Grid>
+                  </div>
+                )}
+              </Grid>
+            </div>
+          </section>
+        }
         <div>
           <h6 style={styledtitle}>Content</h6>
           <Editor
@@ -143,5 +261,4 @@ const CreateArticle = () => {
     </>
   )
 }
-
 export default CreateArticle
