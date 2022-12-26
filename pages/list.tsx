69d6887b9ac4b8b01de8dbcb8e3c5158749f92b8
CeereeC <myself3216@gmail.com>
2022-12-27 03:59:35 +0800

    feat: Add article list table

---
+++ pages/list.tsx
@@ -0,0 +1,12 @@
+import ViewArticleListPage from 'components/ViewArticleListPage'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <ViewArticleListPage />
+    </div>
+  )
+}
+
+export default Home
