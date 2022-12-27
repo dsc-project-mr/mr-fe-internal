b77a6c1660463ba948c600671fe2ee419b2b20ef
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2022-12-27 17:40:11 +0800

    View Campaign List Page (#27)
    * chore: add react-date-range
    
    * feat: working searchbar
    
    * feat: mostly completed searchbar
    
    * feat: add empty selection
    
    * feat: move filters to seperate files
    
    * feat: add filterprops
    
    * feat: add urgency
    
    * fix: yarn prettier
    
    * feat: add defaults for donation
    
    * feat: add docs for searchbar
    
    * chore: add mui data-grid
    
    * feat: add view campaign list
    
    * feat: add nav buttons
    
    * chore: clean up code
    
    * fix: change file name
    
    Co-authored-by: wilinetan <wilinetan26@gmail.com>
    Co-authored-by: Wiline Tan <51109678+wilinetan@users.noreply.github.com>

---
+++ pages/campaigns/donations/index.tsx
@@ -0,0 +1,90 @@
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import { DataGrid } from '@mui/x-data-grid'
+import DocumentListTabs from 'components/DocumentListTabs'
+import Searchbar from 'components/Searchbar'
+import { donationFilters } from 'components/Searchbar/defaults'
+import { DocumentStatus } from 'constants/DocumentStatus'
+import { CampaignStatus } from 'constants/Donation'
+import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+import { Campaign, campaignColumns } from 'models/campaign'
+
+const testData: Campaign[] = [
+  {
+    id: 1,
+    name: 'Impact Fund',
+    details: 'Details of Impact Fund go here',
+    donors: 15,
+    amount: 15,
+    country: 'Singapore',
+    status: CampaignStatus.PUBLISHED,
+  },
+  {
+    id: 2,
+    name: 'Impact Fund v2',
+    details: 'Details of Impact Fund v2 go here',
+    donors: 10,
+    amount: 200,
+    country: 'Singapore',
+    status: CampaignStatus.ARCHIVED,
+  },
+  {
+    id: 3,
+    name: 'Impact Fund v3',
+    details: 'Details of Impact Fund v3 go here',
+    donors: 19,
+    amount: 50000,
+    country: 'Singapore',
+    status: CampaignStatus.DRAFT,
+  },
+]
+
+const CampaignList: NextPage = () => {
+  // TODO get this from a API call
+  const tags = ['food', 'clothes', 'mental-health', 'financial', 'training']
+
+  // TODO: we might need to have 4 diff data storing, otherwise we will have to keep
+  // recalling the APIs when we switch tabs
+  const [campaigns, setCampaigns] = useState<Campaign[]>([])
+  const [search, setSearch] = useState<string>('')
+  const [status, setStatus] = useState<DocumentStatus>(DocumentStatus.All)
+  const { props: _props, filters } = donationFilters(tags)
+
+  useEffect(() => {
+    const props = { ..._props, status }
+    setCampaigns(testData)
+    // setCampaigns(someApi(search, props))
+  }, [_props, status, search])
+
+  const handleView = (row: Campaign) => {
+    // route(`/donation/view/${row.id}`, row)
+  }
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Typography sx={{ marginTop: 2, marginBottom: 2 }} variant="h4">
+        Donations
+      </Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Button color="secondary" variant="contained">
+          Create New Campaign
+        </Button>
+        <Searchbar search={search} setSearch={setSearch} filters={filters} />
+      </Box>
+      <Box sx={{ flex: 1 }}>
+        <DocumentListTabs status={status} setStatus={setStatus}>
+          <DataGrid
+            columns={campaignColumns(handleView)}
+            rows={campaigns}
+            autoHeight
+            pageSize={10}
+          />
+        </DocumentListTabs>
+      </Box>
+    </Box>
+  )
+}
+
+export default CampaignList
