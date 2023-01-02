63dc4179c5aedce654632b79656ae9f0f6986c55
Li Hui Cham <lihuicham@gmail.com>
2023-01-02 13:43:12 +0800

    Add CreateButton

---
+++ pages/CreateNewUserAccount/CreateButton.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+
+const CreateButton = () => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+    return (
+        <div>
+        <Button variant="contained" onClick={handleClickOpen}>
+          CREATE
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title" variant="h5">
+            {"Create New User?"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Are you sure that you would like to create this new user ---- ?
+              The email cannot be changed after creation. 
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose} autoFocus>
+              Create
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    )
+}
+
+export default CreateButton; 
+
--- pages/CreateNewUserAccount/CreateNewUserAccount.tsx
+++ pages/CreateNewUserAccount/CreateNewUserAccount.tsx
@@ -1,17 +1,30 @@
-import { Typography, Box, TextField, Stack } from "@mui/material";
+import * as React from 'react';
+import { Typography, Box, TextField, Stack, Button } from "@mui/material";
 import SelectRoleTextField from "./SelectRoleTextField";
 import UploadProfileImage from "./UploadProfileImage";
+import CreateButton from "./CreateButton";
+
 
 const CreateNewUserAccount = () => {
+
+    const usernameRef = React.useRef();
+
     return (
         <Box>
             <Typography variant="h4" sx={styledTitle}>Create New User Account</Typography>
             <Stack spacing={10} sx={styledStack}>
                 <UploadProfileImage />
-                <TextField id="outlined-basic" label="Name of User" variant="outlined" required/>
+                <TextField id="outlined-basic" label="Name of User" variant="outlined" required inputRef={usernameRef}/>
                 <TextField id="outlined-basic" label="Email Address" variant="outlined" required/>
                 <SelectRoleTextField />
+
+                <Stack direction="row" spacing={6}>
+                    <Button variant="contained">BACK</Button>
+                    <CreateButton />
+                    <Typography>{usernameRef.current.value}</Typography>
+                </Stack>
             </Stack>
+
 
         </Box>
 
