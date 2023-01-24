25ed717bbd3a43d07fe516568086fdcc11dbbe82
CeereeC <myself3216@gmail.com>
2023-01-24 16:52:45 +0800

    feat: Add contentUrl redirect for view article page

--- components/ViewArticlePage/index.tsx
+++ components/ViewArticlePage/index.tsx
@@ -1,5 +1,4 @@
 import { Grid, Typography } from '@mui/material'
-import { useState } from 'react'
 import ArticlePanel from './ArticlePanel'
 import ButtonsPanel from './Buttons/ButtonsPanel'
 import DetailsPanel from './DetailsPanel'
