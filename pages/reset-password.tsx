0af298fe4fe45c45bc85a65694e795cb37c25ff0
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2023-01-25 20:13:25 +0800

    feat: reset password

--- pages/reset-password.tsx
+++ pages/reset-password.tsx
@@ -1,37 +1,41 @@
-import type { NextPage } from 'next'
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-// import Image from "next/image";
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import type { NextPageWithLayout } from './_app'
+import CenteredThemeBackground from 'components/CenteredThemeBackground'
 
-// TODO: temp, until we get the MR logo into assets folder
-const MR_LOGO = 'https://www.mercyrelief.org/site/wp-content/uploads/logo-mercyrelief@2x.png';
+const ResetPassword: NextPageWithLayout = () => {
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Typography
+        sx={{ marginTop: 3, marginBottom: 1 }}
+        variant="subtitle1"
+        fontWeight="bold"
+      >
+        Reset Password
+      </Typography>
+      <TextField
+        sx={{ marginTop: 2 }}
+        id="new-password"
+        label="New Password"
+        type="password"
+      />
+      <TextField
+        sx={{ marginTop: 2 }}
+        id="confirm-new-password"
+        label="Confirm New Password"
+        type="password"
+      />
+      <Button sx={{ marginTop: 2 }} variant="contained">
+        Reset Password
+      </Button>
+    </Box>
+  )
+}
 
-const ResetPassword: NextPage = () => {
-    return (
-        /* TODO This minHeight should be unnecessary when the layout is flex*/
-        <Box sx={{ minHeight: '100vh', display: 'flex' }}>
-            <Box sx={{
-                margin: 'auto',
-                display: 'flex',
-                flexDirection: 'column',
-                minWidth: '200px',
-                width: '40%',
-                maxWidth: '600px'
-            }}>
-                <Box sx={{ alignSelf: "center", marginBottom: 5 }}>
-                    {/* TODO: temp, until we get the MR logo into assets folder */}
-                    {/*<Image src={MR_LOGO} layout="fill"/>*/}
-                    <img src={MR_LOGO} height={100}/>
-                </Box>
-                <Typography sx={{ marginTop: 3, marginBottom: 1 }} variant="subtitle1" fontWeight="bold">Request for Password Reset</Typography>
-                <TextField sx={{ marginTop: 2 }} id="new-password" label="New Password" type="password" />
-                <TextField sx={{ marginTop: 2 }} id="confirm-new-password" label="Confirm New Password" type="password" />
-                <Button sx={{ marginTop: 2 }} variant="contained">Send Reset Link</Button>
-            </Box>
-        </Box>
-    )
+ResetPassword.getLayout = (page) => {
+  return <CenteredThemeBackground>{page}</CenteredThemeBackground>
 }
 
 export default ResetPassword
