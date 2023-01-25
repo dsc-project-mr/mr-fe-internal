4570384e5b7bc03d17df2ba5d8bb857032c6e851
Chandrasekaran Akash <enigmatrix2000@gmail.com>
2023-01-25 20:00:06 +0800

    feat: login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,44 +1,40 @@
-import type { NextPage } from 'next'
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Alert from "@mui/material/Alert";
-import Link from "next/link";
-// import Image from "next/image";
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import type { NextPageWithLayout } from './_app'
+import CenteredThemeBackground from 'components/CenteredThemeBackground'
+import Link from 'next/link'
+import Alert from '@mui/material/Alert'
 
-// TODO: temp, until we get the MR logo into assets folder
-const MR_LOGO = 'https://www.mercyrelief.org/site/wp-content/uploads/logo-mercyrelief@2x.png';
-
-const Login: NextPage = () => {
+const Login: NextPageWithLayout = () => {
   return (
-      /* TODO This minHeight should be unnecessary when the layout is flex*/
-      <Box sx={{ minHeight: '100vh', display: 'flex' }}>
-          <Box sx={{
-              margin: 'auto',
-              display: 'flex',
-              flexDirection: 'column',
-              minWidth: '200px',
-              width: '40%',
-              maxWidth: '600px'
-          }}>
-              <Box sx={{ alignSelf: "center", marginBottom: 5 }}>
-                  {/* TODO: temp, until we get the MR logo into assets folder */}
-                  {/*<Image src={MR_LOGO} layout="fill"/>*/}
-                  <img src={MR_LOGO} height={100}/>
-              </Box>
-              <Typography sx={{ marginTop: 3 }} variant="subtitle1" fontWeight="bold">Login</Typography>
-              <TextField sx={{ marginTop: 3 }} id="email" label="Email address"/>
-              <TextField sx={{ marginTop: 2, marginBottom: 1 }} id="password" label="Password" type="password" />
-              <Link href="/ForgotPassword">Forget Password?</Link>
-              <Button sx={{ marginTop: 2 }} variant="contained">SIGN IN</Button>
-              <Alert sx={{ marginTop: 2 }} severity="error">
-                  <Typography variant="subtitle2" fontWeight="bold">Login Failed!</Typography>
-                  <Typography variant="body2">Email address/password not found</Typography>
-              </Alert>
-          </Box>
-      </Box>
+    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <TextField sx={{ marginTop: 12 }} id="email" label="Email Address" />
+      <TextField
+        sx={{ marginTop: 3, marginBottom: 2 }}
+        id="password"
+        label="Password"
+        type="password"
+      />
+      <Link href="/ForgotPassword">Forget Password?</Link>
+      <Button sx={{ marginTop: 3 }} variant="contained">
+        SIGN IN
+      </Button>
+      <Alert sx={{ marginTop: 3 }} severity="error">
+        <Typography variant="subtitle2" fontWeight="bold">
+          Login Failed!
+        </Typography>
+        <Typography variant="body2">
+          Email address/password not found
+        </Typography>
+      </Alert>
+    </Box>
   )
+}
+
+Login.getLayout = (page) => {
+  return <CenteredThemeBackground>{page}</CenteredThemeBackground>
 }
 
 export default Login
