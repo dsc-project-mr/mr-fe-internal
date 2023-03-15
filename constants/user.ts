520795d26e8974bdc7ceef59cafec49ca922b35a
wilinetan <wilinetan26@gmail.com>
2023-03-15 16:10:13 +0800

    remove user.ts

--- constants/user.ts
+++
@@ -1,8 +0,0 @@
-export enum Role {
-  SUPER_ADMIN = 'super admin',
-  DONATION_CAMPAIGN_EDITOR = 'donation campaign editor',
-  ARTICLE_EDITOR = 'article editor',
-  USER = 'internal user',
-}
-
-export const USER_URL = '/user/'
