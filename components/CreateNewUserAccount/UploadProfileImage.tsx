df2c5206b5371332f9104fd12f48d4a1e5f03b40
Li Hui Cham <99931775+lihuicham@users.noreply.github.com>
2023-03-15 15:39:33 +0800

    34 create new user account page internal (#40)
    * Complete TextField
    
    * Added code for UploadProfileImage
    
    * Add CreateButton
    
    * Completed CreateNewUserAccount page
    
    * Removed package-lock.json
    
    * Clean up code
    
    * Attempt POST requests
    
    * update select-role component
    
    * Fixed modal and props
    
    * attempt with post request
    
    * feat/disabled button until all fields completed
    
    * fix: calling api for creating user, add notistick for user feedback, add role enum for users
    
    * fix: styling of uploadprofileimage.tsx
    
    * fix: align buttons right
    
    ---------
    
    Co-authored-by: wilinetan <wilinetan26@gmail.com>

---
+++ components/CreateNewUserAccount/UploadProfileImage.tsx
@@ -0,0 +1,72 @@
+import { Box, Avatar, IconButton, Typography } from '@mui/material'
+import * as React from 'react'
+
+const UploadProfileImage = () => {
+  const [image, setImage] = React.useState('')
+  const inputFileRef = React.useRef<HTMLInputElement>(null)
+
+  const cleanUp = () => {
+    URL.revokeObjectURL(image)
+    if (inputFileRef.current) {
+      inputFileRef.current.value = ''
+    }
+  }
+
+  const changeImage = (newImage: string) => {
+    if (image) {
+      cleanUp()
+    }
+    setImage(newImage)
+  }
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newImage = event.target?.files?.[0]
+
+    if (newImage) {
+      changeImage(URL.createObjectURL(newImage))
+    }
+  }
+
+  return (
+    <Box>
+      <input
+        hidden
+        accept="image/*"
+        id="contained-button-file"
+        type="file"
+        onChange={handleOnChange}
+        ref={inputFileRef}
+      />
+      <label htmlFor="contained-button-file">
+        <IconButton component="span">
+          <Avatar
+            sx={profileImageStyled}
+            src={image || '/static/default-profile-pic.jpeg'}
+          />
+          <Typography sx={overlayStyled}>Edit Image</Typography>
+        </IconButton>
+      </label>
+    </Box>
+  )
+}
+
+const profileImageStyled = {
+  width: '212px',
+  height: '212px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const overlayStyled = {
+  position: 'absolute',
+  bottom: 20,
+  background: 'rgba(0, 0, 0, 0.5)',
+  color: '#f1f1f1',
+  width: '80%',
+  height: '13%',
+  fontSize: '18px',
+  textAlign: 'center',
+}
+
+export default UploadProfileImage
